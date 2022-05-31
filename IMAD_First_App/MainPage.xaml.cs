d67248a30c3a4cd783e24c66214fdd114604532d
Zackary <zspink@nea.k12.ar.us>
2022-05-31 13:30:05 -0500

    Initial commit

---
+++ IMAD_First_App/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace IMAD_First_App;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnMagentaClicked(object sender, EventArgs e)
+	{
+		scrollView.BackgroundColor = Color.Parse("Magenta");
+	}
+
+    private void OnOrangeClicked(object sender, EventArgs e)
+    {
+        scrollView.BackgroundColor = Color.Parse("Orange");
+    }
+
+    private void OnResetClicked(object sender, EventArgs e)
+    {
+        scrollView.BackgroundColor = Color.Parse("White");
+    }
+}
+
---
+++ IMAD_First_App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace IMAD_First_App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
